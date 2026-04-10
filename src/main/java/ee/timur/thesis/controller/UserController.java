9a04c151c3070028b7100f056e2be75192ba3e09
timur0108 <timur.sirazitdinov@gmail.com>
2026-04-10 15:43:55 +0300

    edt

--- src/main/java/ee/timur/thesis/controller/UserController.java
+++ src/main/java/ee/timur/thesis/controller/UserController.java
@@ -1,9 +1,11 @@
 package ee.timur.thesis.controller;
 
 import ee.timur.thesis.dto.UserDTO;
+import ee.timur.thesis.model.User;
 import ee.timur.thesis.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class UserController {
     @GetMapping("/all")
     public List<UserDTO> getAll() {
         return userService.getAll();
+    }
+
+    @GetMapping("/committee-members/unsubmitted/{thesisId}")
+    public List<UserDTO> getUnsubmittedCommitteeMembers(@PathVariable Long thesisId) {
+        return userService.getUnsubmittedCommitteeMembers(thesisId);
     }
 }
--- src/main/java/ee/timur/thesis/service/UserService.java
+++ src/main/java/ee/timur/thesis/service/UserService.java
@@ -2,6 +2,8 @@ package ee.timur.thesis.service;
 
 import ee.timur.thesis.dto.UserDTO;
 import ee.timur.thesis.mapper.UserMapper;
+import ee.timur.thesis.model.User;
+import ee.timur.thesis.repository.CommitteeMemberGradeRepository;
 import ee.timur.thesis.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +16,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final CommitteeMemberGradeRepository committeeMemberGradeRepository;
 
     public List<UserDTO> getAll() {
         return userRepository.findAll().stream()
                 .map(userMapper::toDTO)
                 .toList();
+    }
+
+    public List<UserDTO> getUnsubmittedCommitteeMembers(Long thesisId) {
+        return userRepository.findCommitteeMembersByThesis(thesisId).stream()
+                .filter(user -> committeeMemberGradeRepository.findGradeByThesisAndUser(thesisId, user.getId()).isEmpty())
+                .map(userMapper::toDTO)
+                .toList();
+
     }
 }
